026e86258db1338aa69416b48f306d2041971004
Jordan Barnartt <jbarnart@uwaterloo.ca>
2022-09-26 15:52:48 -0400

    Creating and destroying a connection works

--- qualysapi.py
+++ qualysapi.py
@@ -1,23 +1,34 @@
+import configparser
+import dataclasses
+import datetime
 import requests
 import lxml
 
-API_ROOT = "https://qualysapi.qualys.com/api/2.0"
+CONFIG_FILE = 'qualysapi.conf'
+
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+API_ROOT = config['AUTHENTICATION']['api_root']
+CREDENTIALS = {"username": config['AUTHENTICATION']['username'],
+               "password": config['AUTHENTICATION']['password']}
 
 
-class connection:
+class Connection:
     headers = {"X-Requested-With": "qualysapi python package"}
 
     def __init__(self, username, password):
         data = {"action": "login", "username": username, "password": password}
         conn = requests.post(
             API_ROOT + "fo/session/", headers=self.headers, data=data)
-        self.cookies = {"QualysSession": conn.cookies["QualysSession"]}
+        if conn.status_code == requests.codes.ok:
+            self.cookies = {"QualysSession": conn.cookies["QualysSession"]}
+        else:
+            conn.raise_for_status()
 
     def __del__(self):
         data = {"action": "logout"}
         requests.post(API_ROOT + "fo/session/",
                       data=data, cookies=self.cookies)
-        super(self.__del__)
 
     def request(self, path):
         conn = requests.get(path, headers=self.headers, cookies=self.cookies)
@@ -25,4 +36,30 @@ class connection:
 
     def get_scans(self, filter=None, modifiers=None):
         self.scans = []
-        
+        output = self.request("o/scan/?action=list")
+        for scan in output["SCAN_LIST_OUTPUT"]["RESPONSE"]["SCAN_LIST"]:
+            scan_elements = {element.tag.tolower(
+            ): scan[element] for element in scan}
+            self.scans.append(Scan(**scan_elements))
+
+
+@dataclasses.dataclass
+class Scan:
+    ref: str
+    _type: str
+    title: str
+    user_login: str
+    launch_datetime: datetime.datetime
+    duration: datetime.timedelta
+    processed: str
+    target: str
+    id: str = None
+    scan_type: str = None
+    processing_priority: str = None
+    status: str = None
+    asset_group_title_list: str = None
+    option_profile: str = None
+
+
+if __name__ == "__main__":
+    conn = Connection(**CREDENTIALS)
