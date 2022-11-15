0497c7c8a0d8ffb701b3701d0e1a997da938b312
Jordan Barnartt <jbarnart@uwaterloo.ca>
2022-11-15 07:05:43 -0500

    New direction...

---
+++ debug/parse_dtd.py
@@ -0,0 +1,52 @@
+import re
+import string
+
+ELEMENT_RE = re.compile("<!ELEMENT (?P<name>.+) \\((?P<attributes>[\\s\\S]+?)\\)>")
+
+with open("debug/dtd.txt", "r") as f:
+    text = f.read()
+    matches = ELEMENT_RE.findall(text)
+
+classes = {}
+
+for (name, attributes) in matches:
+    parsed = attributes.replace("\n", "").replace(" ", "").lower()
+    parsed = parsed.split(",")
+    classes[name.lower()] = parsed
+
+with open("debug/output.txt", "w") as f:
+    for c in classes:
+        if classes[c][0] != "#pcdata":
+            f.write("\n@dataclasses.dataclass\n")
+            cl = string.capwords(c, sep="_")
+            f.write(f"class {cl}:\n")
+            for a in classes[c]:
+                if a == "is_default?":
+                    pass
+                if a.strip("*?+") in classes and classes[a.strip("*?+")][0] == "#pcdata":
+                    if a.endswith("*"):
+                        x = a.strip("*")
+                        f.write(f"    {x}: Optional[MutableSequence[str]] = None\n")
+                    elif a.endswith("?"):
+                        x = a.strip("?")
+                        f.write(f"    {x}: Optional[str] = None\n")
+                    elif a.endswith("+"):
+                        x = a.strip("+")
+                        f.write(f"    {x}: MutableSequence[str]\n")
+                    else:
+                        f.write(f"    {a}: str\n")
+                elif a.endswith("*"):
+                    x = a.strip("*")
+                    cl = string.capwords(x, sep="_")
+                    f.write(f"    {x}: Optional[MutableSequence[{cl}]] = None\n")
+                elif a.endswith("?"):
+                    x = a.strip("?")
+                    cl = string.capwords(x, sep="_")
+                    f.write(f"    {x}: Optional[{cl}] = None\n")
+                elif a.endswith("+"):
+                    x = a.strip("+")
+                    cl = string.capwords(x, sep="_")
+                    f.write(f"    {x}: MutableSequence[{cl}]\n")
+                else:
+                    cl = string.capwords(a, sep="_")
+                    f.write(f"    {a}: {cl}\n")
--- qualyspy/scan_configurations/option_profiles.py
+++ qualyspy/scan_configurations/option_profiles.py
@@ -76,6 +76,143 @@ class Scan_UDP_Ports:
 
 
 @dataclasses.dataclass
+class Scan_Performance:
+    """Performance options for an option profile."""
+
+    parallel_scaling: Optional[bool] = None
+    """(VM only) True means parallel scaling for scanner appliances is enabled; False
+        means parallel scaling for scanner appliances is disabled.
+    """
+
+    overall_performance: str = "Normal"
+    """Overall scan performance level, one of:
+        "Normal" - Recommended in most cases, well balanced between intensity
+        and speed.
+        "High" - Recommended only when scanning a single IP or small number of
+        IPs, optimized for speed and shorter scan times.
+        "Low" - Recommended if responsiveness for individual hosts and services is
+        low, optimized for low bandwidth network connections and highly utilized
+        networks. May take longer to complete.
+    """
+
+    hosts_to_scan_external: int = 1
+    """Maximum number of hosts to scan in parallel using Qualys cloud (external)
+        scanners.
+    """
+
+    hosts_to_scan_appliances: int = 1
+    """Maximum number of hosts to scan in parallel using Qualys Scanner
+        Appliances, installed on your internal network.
+    """
+
+    processes_to_run_total: int = 1
+    """Maximum number of total processes to run at the same time per host."""
+
+    processes_to_run_http: int = 1
+    """Maximum number of HTTP processes to run at the same time per host."""
+
+    packet_delay: int = 1
+    """The delay between groups of packets sent to each host during a scan."""
+
+    def __post_init__(self) -> None:
+        self._check_parameters()
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        super().__setattr__(__name, __value)
+        if __name == "overall_performance":
+            self._check_parameters()
+
+    def _check_parameters(self) -> None:
+        """Confirms that any value with additional restrictions meets those restrictions."""
+
+        good_overall_performances = ["Normal", "High", "Low"]
+
+        if self.overall_performance not in good_overall_performances:
+            raise ValueError(
+                f"overall_performance: {self.overall_performance} "
+                f"not one of {good_overall_performances}"
+            )
+
+
+@dataclasses.dataclass
+class Scan_Password_Brute_Forcing_Custom:
+    """Settings for a custom password brute force scan.
+
+    Warning:
+        An Import Option Profile API call does not import custom password
+        brute forcing lists regardless of Option Profile XML file content. Please
+        configure using Qualys portal UI."""
+
+    id: str
+    """(VM only) Custom password brute forcing list ID."""
+
+    title: str
+    """(VM only) Custom password brute forcing list title."""
+
+    type_: str
+    """(VM only) Type of custom password brute forcing list, one of: "ftp", "ssh", "windows"."""
+
+    login_password: MutableSequence[str]
+    """(VM only) Login/password list (maximum 50) for custom password brute
+        forcing list."""
+
+    def __post_init__(self) -> None:
+        self._check_parameters()
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        super().__setattr__(__name, __value)
+        if __name == "type_" or __name == "login_password":
+            self._check_parameters()
+
+    def _check_parameters(self) -> None:
+        """Confirms that any value with additional restrictions meets those restrictions."""
+
+        good_types = ["ftp", "ssh", "windows"]
+        if self.type_ not in good_types:
+            raise ValueError(f"type: {self.type_} not one of {good_types}")
+
+        if len(self.login_password) > 50:
+            raise ValueError(
+                f"length of login_password is {len(self.login_password)}; maximum 50"
+            )
+
+
+@dataclasses.dataclass
+class Scan_Password_Brute_Forcing:
+    """Password brute forcing settings in an options profile."""
+
+    has_system: Optional[bool] = None
+    """(VM only) True means system password brute forcing enabled; False means system
+        password brute forcing is not enabled."""
+
+    system_level: Optional[int] = 3
+    """(VM only) System password brute forcing level, one of: 1 (for minimal,
+        empty passwords), 2 (for Limited), 3 (for Standard, up to 60 per login ID), 4
+        (for Exhaustive)."""
+
+    custom_list: MutableSequence[Scan_Password_Brute_Forcing_Custom] = []
+    """"""
+
+    def __post_init__(self) -> None:
+        self._check_parameters()
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        super().__setattr__(__name, __value)
+        if __name == "system_level":
+            self._check_parameters()
+
+    def _check_parameters(self) -> None:
+        """Confirms that any value with additional restrictions meets those restrictions."""
+
+        good_system_levels = [1, 2, 3, 4]
+
+        if self.system_level not in good_system_levels:
+            raise ValueError(
+                f"system_level: {self.system_level} not one of {good_system_levels}"
+            )
+
+
+@dataclasses.dataclass
 class Option_Profile_Scan:
     """A definition of the scan settings for an Option_Profile."""
 
@@ -132,12 +269,29 @@ class Option_Profile_Scan:
          False means Qualys won't purge hosts when OS is changed.
     """
 
+    performance: Optional[Scan_Performance] = None
+    """Performance options for an option profile."""
+
+    port_scanning_and_host_discovery: str = "Normal"
+    """(VM only) The aggressiveness (parallelism) of port scanning and host
+        discovery at the port level: "Normal", "Medium", "Low" or "Minimum". Lowering
+        the intensity level has the effect of serializing port scanning and host
+        discovery."""
+
+    load_balancer_detection: bool = True
+    """(VM only) True means scans will detect load balancers and report in QID
+        86189” False means scans will not detect load balancers."""
+
     def __post_init__(self) -> None:
         self._check_parameters()
 
     def __setattr__(self, __name: str, __value: Any) -> None:
         super().__setattr__(__name, __value)
-        if __name == "standard_scan" or __name == "targetted_scan":
+        if (
+            __name == "standard_scan"
+            or __name == "targetted_scan"
+            or __name == "port_scanning_and_host_discovery"
+        ):
             self._check_parameters()
 
     def _check_parameters(self) -> None:
@@ -145,6 +299,16 @@ class Option_Profile_Scan:
             raise ValueError("one of standard_scan or targetted_scan must be True.")
         elif self.standard_scan and self.targeted_scan:
             raise ValueError("standard_scan and targeted_scan cannot both be True.")
+
+        good_port_scanning_and_host_discoveries = ["Normal", "Medium", "Low", "Minimum"]
+        if (
+            self.port_scanning_and_host_discovery
+            not in good_port_scanning_and_host_discoveries
+        ):
+            raise ValueError(
+                f"port_scanning_and_host_discovery: {self.port_scanning_and_host_discovery} not "
+                f"one of {good_port_scanning_and_host_discoveries}"
+            )
 
 
 @dataclasses.dataclass
@@ -197,5 +361,3 @@ class Option_Profile:
     """(VM only) True means parallel scaling for scanner appliances is enabled; False
         means parallel scaling for scanner appliances is disabled.
     """
-
-    
