ec79c652921d01c4fe2c657ccf4492a9e8d920cf
Jordan Barnartt <jbarnartt@uwaterloo.ca>
2022-12-06 15:36:12 -0500

    Started on Host class

--- debug/quickscan.py
+++ debug/quickscan.py
@@ -23,5 +23,5 @@ def launch_quick_scan(conn, ip):
 
 if __name__ == "__main__":
     conn = qualysapi.Connection()
-    ip = ipaddress.IPv4Address("172.25.40.166")
+    ip = ipaddress.IPv4Address("")
     launch_quick_scan(conn, ip)
