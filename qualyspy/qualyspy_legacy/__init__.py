66e040f32ba0969f62f5f736f441081bdcd50dd8
Jordan Barnartt <jbarnart@uwaterloo.ca>
2023-07-10 10:58:49 -0400

    Remove legacy stuff

--- qualyspy/qualyspy_legacy/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import qualysapi
-from . import qutils
-from . import exceptions
-from .certview import certificate
-from .asset_mgmt_tagging import api_input, asset, tag
-from .assets import host_list_detection, host_list
-from .scan_configuration import knowledgebase
--- qualyspy/qualyspy_legacy/exceptions.py
+++
@@ -1,2 +0,0 @@
-class Qualys_API_Error(Exception):
-    """Exception raised when the Qualys API returns a non-200 response, or some other error."""
